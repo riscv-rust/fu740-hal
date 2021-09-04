ec7c90397948e879661d6b6eaf0ff502b692e035
Vadim Kaushan <admin@disasm.info>
2021-09-04 19:25:58 +0300

    PLL setup code refactoring

--- src/clock.rs
+++ src/clock.rs
@@ -16,61 +16,61 @@ impl PrciExt for PRCI {
     }
 }
 
-pub struct ClockSetup {
-    prci: PRCI,
-    coreclk: Option<u32>,
+struct PLLConfig {
+    r: u8,
+    f: u16,
+    q: u8,
+    range: u8,
+    bypass: bool,
 }
 
-impl ClockSetup {
-    pub fn coreclk<F: Into<Hertz>>(mut self, freq: F) -> Self {
-        self.coreclk = Some(freq.into().0);
-        self
-    }
-
-    pub fn freeze(self) -> Clocks {
-        let coreclk = self.coreclk.unwrap_or(HFXCLK);
-
-        // Switch to HFXCLK
-        unsafe {
-            self.prci
-                .core_clk_sel_reg
-                .modify(|r, w| w.bits(r.bits() | 1));
-            self.prci.core_clk_sel_reg.read(); // barrier
+impl PLLConfig {
+    fn calculate(input: u32, output: u32) -> Result<PLLConfig, &'static str> {
+        if input == output {
+            return Ok(PLLConfig {
+                r: 0,
+                f: 0,
+                q: 0,
+                range: 0,
+                bypass: true,
+            });
         }
 
-        if coreclk == HFXCLK {
-            return Clocks {
-                coreclk,
-                pclk: HFXCLK / 2,
-            };
-        }
-
-        let divq: u8 = match coreclk {
-            1600_000_000..=u32::MAX => panic!("Requested coreclk frequency is too high"),
+        let divq: u8 = match output {
+            f if f > 2400_000_000 => {
+                return Err("Requested PLL output frequency is too high");
+            }
             f if f >= 1200_000_000 => 1,
             f if f >= 600_000_000 => 2,
             f if f >= 300_000_000 => 3,
             f if f >= 150_000_000 => 4,
             f if f >= 75_000_000 => 5,
             f if f >= 37_500_000 => 6,
-            _ => panic!("Requested coreclk frequency is too low"),
+            _ => {
+                return Err("Requested PLL output frequency is too low");
+            }
         };
-        let q = 1 << divq;
-        let vco = (coreclk as u64) * (q as u64);
+        let vco = (output as u64) << divq;
 
         let divr = (0..3)
             .min_by_key(|divr| {
-                let pllin = HFXCLK / (divr + 1);
-                let f1 = vco / (2 * pllin as u64);
-                let vco1 = f1 * 2 * (pllin as u64);
-                ((vco1 as i64) - (vco as i64)).abs()
+                let pllin = input / (divr + 1);
+                if pllin < 7_000_000 || pllin >= 200_000_000 {
+                    i64::MAX
+                } else {
+                    let f1 = vco / (2 * pllin as u64);
+                    let vco1 = f1 * 2 * (pllin as u64);
+                    ((vco1 as i64) - (vco as i64)).abs()
+                }
             })
             .unwrap();
-        let pllin = HFXCLK / (divr + 1);
+        let pllin = input / (divr + 1);
         let divf = (vco / (2 * pllin as u64) - 1) as u16;
 
-        let filter = match pllin {
-            f if f < 7_000_000 => panic!("Invalid PLL input frequency"),
+        let range = match pllin {
+            f if f < 7_000_000 => {
+                return Err("Invalid PLL input frequency");
+            }
             f if f < 11_000_000 => 1,
             f if f < 18_000_000 => 2,
             f if f < 30_000_000 => 3,
@@ -78,38 +78,83 @@ impl ClockSetup {
             f if f < 80_000_000 => 5,
             f if f < 130_000_000 => 6,
             f if f < 200_000_000 => 7,
-            _ => panic!("Invalid PLL input frequency"),
+            _ => {
+                return Err("Invalid PLL input frequency");
+            }
         };
 
-        self.prci.core_pllcfg.modify(|_, w| unsafe {
-            w.bits(0);
-            w.pllr().bits(divr as u8);
-            w.pllf().bits(divf);
-            w.pllq().bits(divq);
-            w.pllrange().bits(filter);
-            w.pllbypass().clear_bit();
-            w.pllfsebypass().set_bit()
-        });
+        Ok(PLLConfig {
+            r: divr as u8,
+            f: divf,
+            q: divq,
+            range,
+            bypass: false,
+        })
+    }
 
-        // Wait for lock
-        while self.prci.core_pllcfg.read().plllock().bit_is_clear() {}
+    fn output_frequency(&self, input: u32) -> u32 {
+        if self.bypass {
+            input
+        } else {
+            let vco = (input as u64) * 2 * (self.f as u64 + 1) / (self.r as u64 + 1);
+            (vco >> self.q) as u32
+        }
+    }
+}
 
-        // Switch to PLL
+pub struct ClockSetup {
+    prci: PRCI,
+    coreclk: Option<u32>,
+}
+
+impl ClockSetup {
+    pub fn coreclk<F: Into<Hertz>>(mut self, freq: F) -> Self {
+        let freq = freq.into().0;
+        assert!(freq < 1600_000_000);
+
+        self.coreclk = Some(freq);
+        self
+    }
+
+    pub fn freeze(self) -> Clocks {
+        let coreclk = self.coreclk.unwrap_or(HFXCLK);
+
+        let core_pll = PLLConfig::calculate(HFXCLK, coreclk).unwrap();
+
         unsafe {
-            self.prci.corepllsel.modify(|r, w| w.bits(r.bits() & !1));
-            self.prci.corepllsel.read(); // barrier
-
+            // Switch to HFXCLK
             self.prci
                 .core_clk_sel_reg
-                .modify(|r, w| w.bits(r.bits() & !1));
-            self.prci.core_clk_sel_reg.read(); // barrier
+                .modify(|r, w| w.bits(r.bits() | 1));
+
+            // Apply PLL configuration
+            self.prci.core_pllcfg.write_with_zero(|w| {
+                w.pllr().bits(core_pll.r);
+                w.pllf().bits(core_pll.f);
+                w.pllq().bits(core_pll.q);
+                w.pllrange().bits(core_pll.range);
+                w.pllbypass().bit(core_pll.bypass);
+                w.pllfsebypass().set_bit()
+            });
+
+            if !core_pll.bypass {
+                // Wait for lock
+                while self.prci.core_pllcfg.read().plllock().bit_is_clear() {}
+
+                // Select corepll
+                self.prci.corepllsel.modify(|r, w| w.bits(r.bits() & !1));
+            }
+
+            if coreclk != HFXCLK {
+                // Select PLL as a core clock source
+                self.prci
+                    .core_clk_sel_reg
+                    .modify(|r, w| w.bits(r.bits() & !1));
+            }
         }
 
-        // Calculate real frequency
-        let vco = (pllin as u64) * 2 * (divf as u64 + 1);
-        let coreclk = (vco >> divq) as u32;
         Clocks {
-            coreclk,
+            coreclk: core_pll.output_frequency(HFXCLK),
             pclk: HFXCLK / 2,
         }
     }
