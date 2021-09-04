feda330307b590fefe10a901b982856aa8e7c9ca
U007D <curly@humanenginuity.com>
2021-09-04 15:17:22 -0700

    export `Error` instead of `error` module

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 #![no_std]
 
-pub use fu740_pac as pac;
-
 pub mod clock;
 pub mod consts;
 pub mod delay;
-pub mod error;
+mod error;
 pub mod prelude;
 pub mod serial;
 pub mod stdout;
 pub mod time;
+
+pub use error::Error;
+pub use fu740_pac as pac;
